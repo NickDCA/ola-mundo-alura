bc44af5cf792679241a891a41dbb92f9e659a951
Nicolas David <nicolasdavid193@gmail.com>
2023-04-01 11:54:50 -0300

    Aula 2

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,33 @@
+import styles from "./Banner.module.css";
+import circuloColorido from "assets/circulo_colorido.png";
+import minhaFoto from "assets/minha_foto.png";
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.apresentacao}>
+        <h1 className={styles.titulo}>Olá, Mundo!</h1>
+        <p className={styles.paragrafo}>
+          Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de
+          Front-end da Alura. Aqui compartilho vários conhecimentos, espero que
+          aprenda algo novo :)
+        </p>
+      </div>
+
+      <div className={styles.imagens}>
+        <img
+          src={circuloColorido}
+          className={styles.circuloColorido}
+          aria-hidden={true}
+          alt=""
+        />
+        <img
+          src={minhaFoto}
+          alt="Foto do Antônio Evaldo sorrindo"
+          className={styles.minhaFoto}
+        />
+      </div>
+    </div>
+  );
+}
