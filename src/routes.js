f0a2c168d371501ed170a6a9cfb4c74e7c03036b
Nicolas David <nicolasdavid193@gmail.com>
2023-04-01 14:50:59 -0300

    aula 3

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,37 @@ import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Menu />
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route index element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
+        {/* 
+        
+            Na rota "/", a estrutura a ser renderizada é:
+
+            <PaginaPadrao>
+                <Inicio/>
+            </PaginaPadrao>
+
+            Na rota "/sobremim", a estrutura a ser renderizada é:
+
+            <PaginaPadrao>
+                <SobreMim/>
+            </PaginaPadrao>
+
+        */}
+
         <Route path="*" element={<div>Página não encontrada</div>} />
       </Routes>
+      <Rodape />
     </BrowserRouter>
   );
 }
